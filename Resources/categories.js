0072e01c904b6c1fa55e8619f311bd8d7c50de40
efaby <efaby10@gmail.com>
2013-05-09 17:02:01 -0500

    Implementacion of categories

--- Resources/categories.js
+++ Resources/categories.js
@@ -1,18 +1,17 @@
 var categoriesWin = function() {
 
-	//creamos la ventana
 	var win = Titanium.UI.createWindow({
 		title : 'Categories',
 		backgroundColor : '#fff'
 	});
 	var table = Ti.UI.createTableView({
-		top: 5,
-		backgroundColor: '#ffffff',
+		top: 5,	
+		separatorColor: "#fff",		
 	});
 	var tableData = [];
 	var client = Ti.Network.createHTTPClient();
 	var domain = 'http://www.ticr.com/';
-	var url = domain + 'index.php?option=com_mobile';
+	var url = domain + 'index.php?option=com_mobile&task=categories';
 	client.open('POST',url);
 	
 	client.onload = function(){
@@ -22,50 +21,27 @@ var categoriesWin = function() {
 		
 		for (var i=0; i < responses.length; i++) {
 			var row = Ti.UI.createTableViewRow({
-			 	height: '60dp',
+			 	height: '70dp',
 			 	touchEnabled: true,
 			 	rowIndex:i,
-			 	link: domain + responses[i].streamer,
-			 	
+			 	link: 'Category id: ' + responses[i].id,
+			 	hasChild: true,
+			 	backgroundColor: '#e0e0e0',		
 			 });
 			 
-			var nameLabel = Ti.UI.createLabel({
-            text:responses[i].title,
-            font:{
-                fontSize:'14dp',
-            	fontWeight:'bold'
-	        	},
-	        height:'auto',
-	        left:'70dp',
-	        top:'5dp',
-	        color:'#000',
-	        link: domain + responses[i].streamer,
-	        });
-	        
-	        var catLabel = Ti.UI.createLabel({
-	        text:'"' + responses[i].category_title + '"',
+			var catLabel = Ti.UI.createLabel({
+	        text: responses[i].category_title,
 	        font:{
-	            fontSize:'12dp'
+	            fontSize:'30dp',
+	            fontWeight:'bold'
 	        },
 	        height:'auto',
-	        left:'75dp',
-	        bottom:'5dp',
+	        left:'15dp',	        
 	        color:'#000',
 	        touchEnabled:false,
-	        link: domain + responses[i].streamer,
-	        });
-        
-        	var image = Ti.UI.createImageView({
-        		image: domain + responses[i].thumbnail,
-        		width: 40,
-        		height: 30,
-        		left: '8dp',
-        		top: '10dp',
-        		link: domain + responses[i].streamer,
-        	});
-                	
-        	row.add(image);
-	        row.add(nameLabel);
+	        link: 'Category id: ' + responses[i].id,
+	        });   
+	           	
 	        row.add(catLabel);        
 	        tableData.push(row);
 		};
@@ -73,6 +49,11 @@ var categoriesWin = function() {
 	//	textField.value = response.name;
 	};
 	client.onerror = function(e){alert('Transmission error: ' + e.error);};
+	
+	table.addEventListener('click', function(e){
+		alert(e.source.link);
+	});
+
 	client.send();
 	win.add(table);
 	return win;
