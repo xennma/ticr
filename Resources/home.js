cc669764dc76f0808eaccca14bb9e437b15805d9
efaby <efaby10@gmail.com>
2013-05-10 17:02:36 -0500

    Implementation de home

--- Resources/home.js
+++ Resources/home.js
@@ -8,11 +8,11 @@ var homeWin = function() {
 	
 	var table = Ti.UI.createTableView({
 		top: 5,
-		backgroundColor: '#ffffff',
+		separatorColor: "#fff",
 	});
 	var tableData = [];
 	var client = Ti.Network.createHTTPClient();
-	var domain = 'http://www.ticr.com/';
+	var domain = 'http://192.168.1.4/ticr/';
 	var url = domain + 'index.php?option=com_mobile';
 	client.open('POST',url);
 	
@@ -23,51 +23,88 @@ var homeWin = function() {
 		
 		for (var i=0; i < responses.length; i++) {
 			var row = Ti.UI.createTableViewRow({
-			 	height: '60dp',
+			 	height: '100dp',
 			 	touchEnabled: true,
 			 	rowIndex:i,
 			 	link: domain + responses[i].streamer,
-			 	
+			 	backgroundColor: '#e0e0e0',
 			 });
 			 
 			var nameLabel = Ti.UI.createLabel({
             text:responses[i].title,
             font:{
                 fontSize:'14dp',
-            	fontWeight:'bold'
-	        	},
+            	fontWeight:'bold',
+    	        	},
 	        height:'auto',
-	        left:'70dp',
+	        left:'110dp',
 	        top:'5dp',
-	        color:'#000',
+	        color: "#6cb1d5",
 	        link: domain + responses[i].streamer,
 	        });
 	        
-	        var catLabel = Ti.UI.createLabel({
-	        text:'"' + responses[i].category_title + '"',
+	        var dateLabel = Ti.UI.createLabel({
+	        text: responses[i].startdate,
 	        font:{
-	            fontSize:'12dp'
+	            fontSize:'12dp',
+	            fontWeight:'bold',
 	        },
 	        height:'auto',
-	        left:'75dp',
-	        bottom:'5dp',
-	        color:'#000',
+	        left:'110dp',
+	        top:'40dp',
+	        color:'#717777',
 	        touchEnabled:false,
 	        link: domain + responses[i].streamer,
 	        });
-        
+	        
+        	var placeLabel = Ti.UI.createLabel({
+	        text: responses[i].location,
+	        font:{
+	            fontSize:'12dp',
+	        },
+	        height:'auto',
+	        left:'110dp',
+	        top:'60dp',
+	        color:'#717777',
+	        touchEnabled:false,
+	        link: domain + responses[i].streamer,
+	        });
         	var image = Ti.UI.createImageView({
         		image: domain + responses[i].thumbnail,
-        		width: 40,
-        		height: 30,
-        		left: '8dp',
+        		width: '98dp',
+        		height: '80dp',
+        		left: '5dp',
         		top: '10dp',
         		link: domain + responses[i].streamer,
         	});
-                	
+        	
+            var guestLabel = Ti.UI.createLabel({
+            text: responses[i].guestText,
+            font:{
+                fontSize:'12dp',
+    	        	},
+	        height:'auto',
+	        left:'130dp',
+	        bottom: '5dp',
+	        color: "#6cb1d5",
+	        link: domain + responses[i].streamer,
+	        });
+	        
+	        var imageGuest = Ti.UI.createImageView({
+        		image: 'images/guest.png',
+        		left:'110dp',
+	        	bottom: '5dp',
+	        	width: '17dp',
+        		height: '17dp',
+        		link: domain + responses[i].streamer,
+        	});
+        	
         	row.add(image);
+        	row.add(imageGuest);
 	        row.add(nameLabel);
-	        row.add(catLabel);        
+	        row.add(dateLabel);  
+	        row.add(placeLabel); 
+	        row.add(guestLabel);       
 	        tableData.push(row);
 		};
 		table.setData(tableData);
